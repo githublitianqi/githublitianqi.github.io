f37105dd9a02e7d9216d25b6392cf26b3fbf7cfd
litianqi <1328235268@qq.com>
2022-08-18 10:29:56 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from '@/components/HelloWorld.vue'
 import TheWelcome from '@/components/TheWelcome.vue'
+console.log('env', import.meta.env);
 </script>
 
 <template>
